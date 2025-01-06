e2074b933f2b248719f573afe00731ca72d9138b
surbhikothari03 <111732299+surbhikothari03@users.noreply.github.com>
2025-01-06 15:10:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
 # Data Source
 data "azurerm_client_config" "current" {}
 
